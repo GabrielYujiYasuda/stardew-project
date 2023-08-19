7903109c69c9393e9dd2bb667ff23f813c49f4fa
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-08-18 21:49:31 -0300

    First commit

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        Made by Gabriel Yasuda & Laura Vital Cacique
+      </footer>
+    </>
+  );
+}
