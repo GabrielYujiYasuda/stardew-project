63a4ecd5865a749d9d2be00caa8d2e5a31a7d8c7
GabrielYujiYasuda <gabrielyyasuda@gmail.com>
2023-08-26 20:06:37 -0300

    Adding List component

---
+++ frontend/src/components/List/index.tsx
@@ -0,0 +1,21 @@
+import "./List.css";
+
+export default function List() {
+  return (
+    <>
+      <div>
+        <ul>
+          <li>
+            <h3>Title</h3>
+            <p>Coments</p>
+          </li>
+
+          <li>
+            <h3>Title</h3>
+            <p>Coments</p>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
